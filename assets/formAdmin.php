6c09c6ce330d5c14a173e4c10d6be7a5f29adc3e
Ina Qirko <ina.qirko@gmail.com>
2015-06-14 01:51:06 +0200

    form fix

--- assets/formAdmin.php
+++ assets/formAdmin.php
@@ -40,59 +40,7 @@
 </nav>
 <div class="container">
     <form id="kerkoStudentetSezon" class="form-horizontal">
-        <div class="well col-sm-4">
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Sezoni</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="" placeholder="Sezoni">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Lenda</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="" placeholder="Lenda">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Dega</label>
-                <div class="col-sm-10">
-                    <input type="datetime" class="form-control" id="" placeholder="Dega">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Viti</label>
-                <div class="col-sm-10">
-                    <input type="number" class="form-control" id="" placeholder="Viti">
-                </div>
-            </div>
-        </div>
-        <div class="well col-sm-4">
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Sezoni</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="" placeholder="Sezoni">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Lenda</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="" placeholder="Lenda">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Dega</label>
-                <div class="col-sm-10">
-                    <input type="datetime" class="form-control" id="" placeholder="Dega">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="">Viti</label>
-                <div class="col-sm-10">
-                    <input type="number" class="form-control" id="" placeholder="Viti">
-                </div>
-            </div>
-        </div>
-        <div class="well col-sm-4">
+        <div class="well col-sm-4 col-sm-offset-4">
             <div class="form-group">
                 <label class="col-sm-2 control-label" for="">Sezoni</label>
                 <div class="col-sm-10">
