0133b33c6787e7f459607889d543c4cfc8559a72
Ina Qirko <ina.qirko@gmail.com>
2015-06-27 15:00:51 +0200

    Created error page, added assets/img

---
+++ assets/error.php
@@ -0,0 +1,47 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logged in: Profesor filani</title>
+
+    <meta name="viewport" content="width = device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="fonts/fontawesome-webfont.ttf" rel="stylesheet">
+
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-2 col-sm-offset-2">
+            <div class="error-template">
+                <img class="img-responsive" src="img/fella-not-found.png">
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="error-template">
+                <h1>Oops!</h1>
+                <h2>404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                </div>
+                <div class="error-actions">
+                    <a href="#" class="btn btn-primary btn-lg"><span class="fa fa-home"></span>
+                        Take Me Home
+                    </a>
+                    <a href="#" class="btn btn-default btn-lg"><span class="fa fa-envelope-o"></span>
+                        Contact Support
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
