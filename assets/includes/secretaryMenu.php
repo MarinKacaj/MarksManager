274836c243384846235cd1e001f6a6157135d657
MarinKacaj <m93_vkacaj@yahoo.com>
2015-06-28 19:54:02 +0200

    Removed results from Secretary menu.

--- assets/includes/secretaryMenu.php
+++ assets/includes/secretaryMenu.php
@@ -60,11 +60,6 @@ $baseURL = get_assets_base_url();
     </a>
 </li>
 <li>
-    <a href="<?php echo $baseURL . 'result/list.php' ?>">
-        Rezultat
-    </a>
-</li>
-<li>
     <a href="<?php echo $baseURL . 'season/list.php' ?>">
         Sezon
     </a>
