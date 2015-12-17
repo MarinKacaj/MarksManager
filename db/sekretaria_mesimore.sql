28706a866405db11a3d413121382d54a6cf3455e
MarinKacaj <m93_vkacaj@yahoo.com>
2015-12-17 22:20:08 +0100

    Replaced wrong db with the correct one.

--- db/sekretaria_mesimore.sql
+++ db/sekretaria_mesimore.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2015 at 09:56 PM
+-- Generation Time: Dec 17, 2015 at 10:10 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
